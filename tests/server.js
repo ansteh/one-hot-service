2bbe0d8b5d53aca01b664341d3dd63bbbbaa3022
ansteh <stehle.andre@googlemail.com>
2016-12-19 23:55:28 +0100

    update

--- tests/server.js
+++ tests/server.js
@@ -6,7 +6,10 @@ const config = require('../server/config.js');
 let port = config.port || 3000;
 
 got.post(`http://localhost:${port}/encode/1`, {
-  body: {
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
   	"input": [
   	  {
   	    "user": { "id": 1, "name": "Dalton", "prename": "John", "online": true },
@@ -17,11 +20,27 @@ got.post(`http://localhost:${port}/encode/1`, {
   	    "project": { "id": 2, "name": "docdown"}
   	  }
   	]
-  }
+  })
 })
 .then(response => {
-    console.log(response.body);
+  console.log('success', response.body);
 })
 .catch(error => {
-  console.log(error.response.body);
+  console.log('error', error.response.body);
+});
+
+got.post(`http://localhost:${port}/decode/1`, {
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+	   "input": [[ 1, 0, 0, 2, 1, 0 ],
+  			       [ 1, 0, 0, 2, 2, 1 ]]
+  })
+})
+.then(response => {
+  console.log('success', response.body);
+})
+.catch(error => {
+  console.log('error', error.response.body);
 });
